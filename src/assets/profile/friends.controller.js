d4661a6d49ec7d59521304e70adbbaac07fb6669
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-06-11 14:48:18 -0500

    friends appear on myFriends page

--- src/assets/profile/friends.controller.js
+++ src/assets/profile/friends.controller.js
@@ -4,13 +4,15 @@ angular
     var main = this;
     var fb = new Firebase(API_URL);
     var userData = fb.getAuth();
+    main.friendProfile = [];
 
     Profile.getFriends(userData.uid, function(res) {
       main.friendObj = res;
+      console.log(res);
       for (var friend in main.friendObj) {
         Profile.getOne(main.friendObj[friend], function(arg) {
-          console.log(arg)
-          main.friendProfile = arg;
+          main.friendProfile.push(arg);
+          console.log(arg);
         })
       }
     })
