cfe877416bce13ffa84d7f134015337ad29148d1
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-06-11 10:02:39 -0500

    fixed login functions

--- src/assets/profile/profile.factory.js
+++ src/assets/profile/profile.factory.js
@@ -2,9 +2,9 @@ angular
   .module('twitterFace')
   .factory('Profile', function($http, API_URL) {
     return {
-      create(data, cb) {
+      create(data, uid, cb) {
         $http
-            .put(`${API_URL}/profile.json`, data)
+            .put(`${API_URL}/profile/${uid}.json`, data)
             .success(cb);
       }
     }
