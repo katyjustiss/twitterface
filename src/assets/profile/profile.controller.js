1634d957f59a5158d5834d560145bb217de8673f
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-06-12 12:10:55 -0500

    friends faded out from all users... IT WORKS

--- src/assets/profile/profile.controller.js
+++ src/assets/profile/profile.controller.js
@@ -4,33 +4,16 @@ angular
     var main = this;
 
     Profile.getAll(function(people){
+      //gets all users and gives to ng-repeat
       main.people = people;
+      //creates an array that ng-repeat uses to check who I'm friends with
+      Profile.getFriends($rootScope.auth.uid, function(res){
+        main.friendObj = res;
+        main.friendArr = [];
+        for (var friend in main.friendObj){
+          main.friendArr.push(main.friendObj[friend]);
+        }
+      })
     })
-
-    // ===========TRYING TO FADE OUT FRIENDS FROM ALL FRIENDS============
-    // main.pictures = [];
-    // main.people = [];
-    // Profile.getFriends($rootScope.auth.uid, function(res){
-    //   main.friendObj = res;
-    //   console.log(res);
-
-    //   Profile.getAll(function(people){
-    //   console.log(people);
-
-    //   for(var onePerson in people){
-    //     debugger;
-    //     for (var friend in main.friendObj){
-    //       if(main.friendObj[friend] === onePerson){
-    //         main.pictures.push(people[onePerson]);
-    //       }else if(main.friendObj[friend] !== onePerson && main.people.indexOf(people[onePerson]) === -1 && main.pictures.indexOf(people[onePerson]) === -1){
-    //         main.people.push(people[onePerson]);
-    //       }
-    //     }
-    //   }
-    // });
-
-    // })
-
-
 
 })
